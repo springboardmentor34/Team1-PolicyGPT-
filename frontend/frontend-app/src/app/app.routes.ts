d39f2f3db88d446eb097a93767faf1705b97e026
Dhanusha Ravi <dhanusharavi14@gmail.com>
2026-07-30 21:16:00 +0530

    Initialize frontend Angular application

--- frontend/frontend-app/src/app/app.routes.ts
+++ frontend/frontend-app/src/app/app.routes.ts
@@ -1,3 +1,34 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Login } from './pages/login/login';
+import { Register } from './pages/register/register';
+import { CitizenDashboard } from './pages/citizen-dashboard/citizen-dashboard';
+import { GovernmentDashboard } from './pages/government-dashboard/government-dashboard';
+import { PolicySearch } from './pages/policy-search/policy-search';
+import { PolicyDetails } from './pages/policy-details/policy-details';
+import { SchemeDetails } from './pages/scheme-details/scheme-details';
+import { EligibilityChecker } from './pages/eligibility-checker/eligibility-checker';
+import { Reports } from './pages/reports/reports';
+import { Notifications } from './pages/notifications/notifications';
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+
+  { path: 'login', component: Login },
+  { path: 'register', component: Register },
+
+  { path: 'citizen-dashboard', component: CitizenDashboard },
+  { path: 'government-dashboard', component: GovernmentDashboard },
+
+  { path: 'policy-search', component: PolicySearch },
+  { path: 'policy-details', component: PolicyDetails },
+  { path: 'scheme-details', component: SchemeDetails },
+
+  { path: 'eligibility-checker', component: EligibilityChecker },
+
+  { path: 'reports', component: Reports },
+
+  { path: 'notifications', component: Notifications },
+
+  { path: '**', redirectTo: 'login' }
+];

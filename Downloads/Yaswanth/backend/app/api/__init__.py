6b65201adb377a6f80faf5619ffd914bb0ff8165
K YASWANTH REDDY <o210506@rguktong.ac.in>
2026-08-09 18:53:06 +0530

    Delete Downloads/Yaswanth directory

--- Downloads/Yaswanth/backend/app/api/__init__.py
+++
@@ -1,6 +0,0 @@
-from app.api.auth import router as auth_router
-from app.api.users import router as users_router
-from app.api.policies import router as policies_router
-from app.api.schemes import router as schemes_router
-
-__all__ = ["auth_router", "users_router", "policies_router", "schemes_router"]
